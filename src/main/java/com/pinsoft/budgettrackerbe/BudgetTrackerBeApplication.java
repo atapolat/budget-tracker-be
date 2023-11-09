ca3118adf1771cb3334e4f117de0fdd8afac67a4
atapolat <atapolat.ozkan10@gmail.com>
2023-11-09 15:14:42 +0300

    Initial commit

---
+++ src/main/java/com/pinsoft/budgettrackerbe/BudgetTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.pinsoft.budgettrackerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerBeApplication.class, args);
+	}
+
+}
